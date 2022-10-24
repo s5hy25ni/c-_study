07c63f4536a692008b89386181f737e73c83d3bb
s5hy25ni <s5hy25ni@gmail.com>
2022-10-24 20:34:00 +0900

    2022-10-24 study

---
+++ header1.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace header1 {
+	int foo() {
+		std::cout << "Hi" << std::endl 
+				<< "my name is " 
+				<< "sohyeon" << std::endl;
+		return 0;
+	}
+}
---
+++ header2.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+
+namespace header2 {
+	int foo() {
+		std::cout << "world!" << std::endl;
+		return 0;
+	}
+}
