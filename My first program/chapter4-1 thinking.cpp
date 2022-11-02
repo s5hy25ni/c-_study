729dd972fbc51fc8248255e6716884cbbdf7d577
s5hy25ni <s5hy25ni@gmail.com>
2022-11-02 09:52:08 +0900

    2022-11-01

--- My first program/chapter4-1 thinking.cpp
+++ My first program/chapter4-1 thinking.cpp
@@ -1,45 +1,45 @@
 #include <iostream>
 
-int input_year, input_month, input_day, select_num;
+/*
+int input_year, input_month, input_day;
 
-void input_not_int() {
+int cheakLeapYear(int year) {
+	// 윤년이면
+	if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
+	return 1;
+	}
+	else if (year % 4 == 0 && year % 100 != 0) {
+	return 1;
+	}
+	// 평년이면
+	else return 0;
+}
+
+void cheakInputError() {
 	std::cout << std::endl;
 	std::cout << "정수만 입력하세요." << std::endl << std::endl;
 	std::cin.clear();
 	std::cin.ignore(256, '\n');
 }
-void get_input_year() {
+void getInputYear() {
 	std::cout << "연도 : ";
 	std::cin >> input_year;
 }
-void get_input_month() {
+void getInputMonth() {
 	std::cout << "월 : ";
 	std::cin >> input_month;
 }
-void get_input_day() {
+void getInputDay() {
 	std::cout << "일 : ";
 	std::cin >> input_day;
 	std::cout << std::endl;
 }
 
-int cheak_year(int year) {
-	if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
-		return 1;
-	}
-	else if (year % 4 == 0 && year % 100 == 0) {
-		return 0;
-	}
-	else if (year % 4 == 0) {
-		return 1;
-	}
-	else return 0;
-}
-
-void get_input_date() {
+void cheakInputDate() {
 	while (true) {
-		get_input_year();
+		getInputYear();
 		if (std::cin.fail()) {
-			input_not_int();
+			cheakInputError();
 		}
 		else if (input_year <= 0) {
 			std::cout << "0 이상의 정수를 입력하세요." << std::endl;
@@ -49,9 +49,9 @@ void get_input_date() {
 	}
 
 	while (true) {
-		get_input_month();
+		getInputMonth();
 		if (std::cin.fail()) {
-			input_not_int();
+			cheakInputError();
 		}
 		else if (input_month < 1 || input_month>12) {
 			std::cout << "1과 12 사이의 정수를 입력하세요." << std::endl;
@@ -61,29 +61,28 @@ void get_input_date() {
 	}
 
 	while (true) {
-		int input_end = 1;
-		get_input_day();
+		getInputDay();
 		if (std::cin.fail()) {
-			input_not_int();
+			cheakInputError();
+			continue;
 		}
 		else {
 			switch (input_month) {
 			case 2:
-				if (cheak_year(input_year)) { // 윤년이면
+				if (cheakLeapYear(input_year)) { // 윤년이면
 					if (1 > input_day || 29 < input_day) {
 						std::cout << "윤년입니다. 1과 29 사이의 정수를 입력하세요." << std::endl;
 						std::cout << std::endl;
-						input_end = 0;
+						continue;
 					}
 				}
 				else { // 평년이면
 					if (1 > input_day || 28 < input_day) {
 						std::cout << "평년입니다. 1과 28 사이의 정수를 입력하세요." << std::endl;
 						std::cout << std::endl;
-						input_end = 0;
+						continue;
 					}
 				}
-				break;
 
 			case 4:
 			case 6:
@@ -92,20 +91,18 @@ void get_input_date() {
 				if (1 > input_day || 30 < input_day) {
 					std::cout << "1과 30 사이의 정수를 입력하세요." << std::endl;
 					std::cout << std::endl;
-					input_end = 0;
+					continue;
 				};
-				break;
 
 			default:
 				if (1 > input_day || 31 < input_day) {
 					std::cout << "1과 31 사이의 정수를 입력하세요." << std::endl;
 					std::cout << std::endl;
-					input_end = 0;
+					continue;
 				};
-				break;
 			}
 		}
-		if (input_end) break;
+		return;
 	}
 }
 
@@ -114,142 +111,70 @@ class Date {
 	int month_; // 1부터 12까지
 	int day_; // 1부터 31까지
 
-	int cal_month;
-	int cal_day;
-	int cal_end;
-
-	void cheak_month(int inc) {
-		while (true) {
-			cal_end = 1;
-			switch (month_) {
-			case 2:
-				if (cheak_year(year_)) {
-					if (cal_day <= 29) {
-						day_ = cal_day;
-					}
-					else {
-						cal_day -= 29;
-						AddMonth(1);
-						cal_end = 0;
-					}
-				}
-				else {
-					if (cal_day <= 28) {
-						day_ = cal_day;
-					}
-					else {
-						cal_day -= 28;
-						AddMonth(1);
-						cal_end = 0;
-					}
-				}
-				break;
-			case 4:
-			case 6:
-			case 9:
-			case 11:
-				if (cal_day <= 30) {
-					day_ = cal_day;
-				}
-				else {
-					cal_day -= 30;
-					AddMonth(1);
-					cal_end = 0;
-				}
-				break;
-			default:
-				if (cal_day <= 31) {
-					day_ = cal_day;
-				}
-				else {
-					cal_day -= 31;
-					AddMonth(1);
-					cal_end = 0;
-				}
-				break;
-			}
-			if (cal_end) break;
-		}
-	}
-
 public:
-	void SetDate(int year, int month, int day) {
-		year_ = year;
-		month_ = month;
-		day_ = day;
-	};
-	void AddDay(int inc) {
-		while (true) {
-			if (cheak_year(year_)) {
-				if (inc > 366) {
-					year_ += 1;
-					inc -= 366;
-					continue;
-				}
-				else break;
-			}
-			else {
-				if (inc > 365) {
-					year_ += 1;
-					inc -= 365;
-					continue;
-				}
-				else break;
-			}
-		}
-		
-		cal_day = day_ + inc;
-		while (true) {
-			if (cheak_year(year_)) {
-				if (cal_day > 366) {
-					AddYear(1);
-					cal_day -= 366;
-					continue;
-				}
-				else {
-					cheak_month(cal_day);
-					break;
-				}
-			}
-			else {
-				if (cal_day > 365) {
-					AddYear(1);
-					cal_day -= 365;
-					continue;
-				}
-				else {
-					cheak_month(cal_day);
-					break;
-				}
-			}
-		}
-	};
-	void AddMonth(int inc) {
-		AddYear(inc / 12);
-		inc %= 12;
-		cal_month = month_ + inc;
-		if (cal_month <= 12) month_ = cal_month;
-		else {
-			AddYear(1);
-			month_ = cal_month - 12;
-		}
-	};
-	void AddYear(int inc) {
-		year_ += inc;
-	};
+	void SetDate(int year, int month, int day);
 
-	void ShowDate() {
-		std::cout << "현재 날짜는 " << year_ << "년 " << month_ << "월 " << day_ << "일 입니다." << std::endl << std::endl;
-	}
+	int GetCurrentMonthTotalDays(int year, int month);
+
+	void AddDay(int inc);
+	void AddMonth(int inc);
+	void AddYear(int inc);
+
+	void ShowDate();
 };
+
+void Date::SetDate(int year, int month, int day) {
+	year_ = year;
+	month_ = month;
+	day_ = day;
+}
+
+int Date::GetCurrentMonthTotalDays(int year, int month) {
+	static int month_day[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	if (month != 2) {
+		return month_day[month - 1];
+	}
+	else if (cheakLeapYear(year)) {
+		return 29;
+	}
+	else return 28;
+}
+
+void Date::AddDay(int inc) {
+	while (true) {
+		int current_month_total_days = GetCurrentMonthTotalDays(year_, month_);
+
+		if (day_ + inc <= current_month_total_days) {
+			day_ += inc;
+			return;
+		}
+		else {
+			inc -= (current_month_total_days - day_ + 1);
+			day_ = 1;
+			AddMonth(1);
+		}
+	}
+}
+
+void Date::AddMonth(int inc) {
+	AddYear((inc + month_ - 1) / 12);
+	month_ = month_ + inc % 12;
+	month_ = (month_ == 12 ? 12 : month_ % 12);
+}
+
+void Date::AddYear(int inc) { year_ += inc; }
+
+void Date::ShowDate() {
+	std::cout << "현재 날짜 : " << year_ << "-" << month_ << "-" << day_ << std::endl << std::endl;
+}
 
 int main() {
 
 	Date currentDate;
-	int plus_num;
+	int plus_num, select_num;
 
 	std::cout << "=============== 날짜를 입력해주세요. ===============" << std::endl << std::endl;
-	get_input_date();
+	cheakInputDate();
 	currentDate.SetDate(input_year, input_month, input_day);
 
 	while (true) {
@@ -266,14 +191,14 @@ int main() {
 		std::cout << std::endl;
 
 		if (std::cin.fail()) {
-			input_not_int();
+			cheakInputError();
 			continue;
 		}
 
 		switch (select_num) {
 			case 1:
 				std::cout << "=============== [ 1. 현재 날짜 바꾸기 ] ===============" << std::endl << std::endl;
-				get_input_date();
+				cheakInputDate();
 				currentDate.SetDate(input_year, input_month, input_day);
 				continue;
 
@@ -283,7 +208,7 @@ int main() {
 				std::cin >> plus_num;
 				std::cout << std::endl;
 				if (std::cin.fail()) {
-					input_not_int();
+					cheakInputError();
 				}
 				else if (plus_num < 0) {
 					std::cout << "더하기만 가능합니다." << std::endl << std::endl;
@@ -299,7 +224,7 @@ int main() {
 				std::cin >> plus_num;
 				std::cout << std::endl;
 				if (std::cin.fail()) {
-					input_not_int();
+					cheakInputError();
 				}
 				else if (plus_num < 0) {
 					std::cout << "더하기만 가능합니다." << std::endl << std::endl;
@@ -312,9 +237,8 @@ int main() {
 				std::cout << "=============== [ 4. 일 더하기 ] ===============" << std::endl << std::endl;
 				std::cout << "더하고 싶은 일수를 입력하세요. : ";
 				std::cin >> plus_num;
-				std::cout << std::endl;
 				if (std::cin.fail()) {
-					input_not_int();
+					cheakInputError();
 				}
 				else if (plus_num < 0) {
 					std::cout << "더하기만 가능합니다." << std::endl << std::endl;
@@ -334,3 +258,5 @@ int main() {
 		break;
 	}
 }
+
+*/
--- My first program/chapter4-5.cpp
+++ My first program/chapter4-5.cpp
@@ -5,6 +5,7 @@
 class MyString {
 	char* string_content; // 문자열 데이터를 가리키는 포인터
 	int string_length; // 문자열 길이
+	int memory_capacity; // 현재 할당된 용량
 
 	public:
 		// 문자 하나로 생성
@@ -19,17 +20,27 @@ class MyString {
 		~MyString();
 
 		int length() const;
+		int capacity() const;
+		void reserve(int size);
+
 		void print() const;
 		void println() const;
+
+		MyString& assign(const MyString& str);
+		MyString& assign(const char* str);
+
 };
 
 MyString::MyString(char c) {
 	string_content = new char[1];
 	string_content[0] = c;
+	memory_capacity = 1;
+	string_length = 1;
 }
 
 MyString::MyString(const char* str) {
 	string_length = strlen(str);
+	memory_capacity = string_length;
 	string_content = new char[string_length];
 
 	for (int i = 0; i != string_length; i++) {
@@ -39,6 +50,7 @@ MyString::MyString(const char* str) {
 
 MyString::MyString(const MyString& str) {
 	string_length = str.string_length;
+	memory_capacity = str.string_length;
 	string_content = new char[string_length];
 
 	for (int i = 0; i != string_length; i++) {
@@ -63,12 +75,62 @@ void MyString::println() const {
 	std::cout << std::endl;
 }
 
+MyString& MyString::assign(const MyString& str) {
+	if (str.string_length > memory_capacity) {
+		// 다시 할당
+		delete[] string_content;
+
+		string_content = new char[str.string_length];
+		memory_capacity = str.string_length;
+	}
+	
+	for (int i = 0; i != str.string_length; i++) {
+		string_content[i] = str.string_content[i];
+	}
+
+	string_length = str.string_length;
+
+	return *this;
+}
+
+MyString& MyString::assign(const char* str) {
+	int str_length = strlen(str);
+	if (str_length > memory_capacity) {
+		delete[] string_content;
+
+		string_content = new char[str_length];
+		memory_capacity = str_length;
+	}
+	for (int i = 0; i != str_length; i++) {
+		string_content[i] = str[i];
+	}
+
+	string_length = str_length;
+
+	return *this;
+}
+
+int MyString::capacity() const { return memory_capacity; }
+void MyString::reserve(int size) {
+	if (size > memory_capacity) {
+		char* prev_string_content = string_content;
+
+		string_content = new char[size];
+		memory_capacity = size;
+
+		for (int i = 0; i != string_length; i++) {
+
+		}
+	}
+}
+
 int main() {
 	MyString str1("hello world!");
 	MyString str2(str1);
 
 	str1.println();
 	str2.println();
-}
 
+	
+}
 */
