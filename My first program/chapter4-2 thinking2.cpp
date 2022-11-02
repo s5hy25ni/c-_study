88c6f40dc513025acc662a549a6590427fe8b97f
s5hy25ni <s5hy25ni@gmail.com>
2022-11-02 20:25:06 +0900

    2022-11-02

--- My first program/chapter4-2 thinking2.cpp
+++ My first program/chapter4-2 thinking2.cpp
@@ -1,35 +1,60 @@
-//class Point { // 점에 관한 정보
-//	int x, y;
-//
-//	public:
-//
-//		Point(int pos_x, int pos_y);
-//		int getX() { return x; }
-//		int getY() { return y; }
-//};
-//
-//class Geometry { // 점들을 가지고 연산
-//	public:
-//		Geometry() {
-//			num_points = 0;
-//		}
-//
-//		void AddPoint(const Point& point) {
-//			//point_array[num_points++] = new Point(point.getX(), point.getY());
-//		}
-//
-//		// 모든 점들 간의 거리를 출력하는 함수입니다.
-//		void PrintDistance();
-//
-//		// 모든 점들을 잇는 직선들 간의 교점의 수를 출력해주는 함수
-//		// 참고적으로 임의의 두 점을 잇는 직선의 방정식을 f(x,y) = ax+by+c = 0
-//		// 이라고 할 때 임의의 다른 두 점 (x1, y1)과 (x2, y2)가 f(x, y)=0 을 기준으로
-//		// 서로 다른 부분에 있을 조건은 f(x1, y1) * f(x2, y2) <= 0 이면 됩니다.
-//
-//		void PrintNumMeets();
-//
-//	private:
-//		// 점 100개를 보관하는 배열
-//		Point* point_array[100];
-//		int num_points;	
-//};
+/*
+#include <iostream>
+
+class Point { // 점에 관한 정보
+	int x, y;
+
+	public:
+
+		Point(int pos_x, int pos_y);
+		
+		int getX() { return x; }
+		int getY() { return y; }
+
+};
+
+Point::Point(int pos_x, int pos_y) {
+	x = pos_x;
+	y = pos_y;
+}
+
+class Geometry { // 점들을 가지고 연산
+	public:
+		Geometry() {
+			num_points = 0;
+		}
+		// void addPoint const Point &point ????
+		void addPoint(Point &point) {
+			point_array[num_points ++] = new Point(point.getX(), point.getY());
+		}
+
+		// 모든 점들 간의 거리를 출력하는 함수입니다.
+		void printDistance();
+
+		// 모든 점들을 잇는 직선들 간의 교점의 수를 출력해주는 함수
+		// 참고적으로 임의의 두 점을 잇는 직선의 방정식을 f(x,y) = ax+by+c = 0
+		// 이라고 할 때 임의의 다른 두 점 (x1, y1)과 (x2, y2)가 f(x, y)=0 을 기준으로
+		// 서로 다른 부분에 있을 조건은 f(x1, y1) * f(x2, y2) <= 0 이면 됩니다.
+		void printnummeets();
+
+	private:
+		// 점 100개를 보관하는 배열
+		Point* point_array[100];
+		int num_points;	
+};
+
+void Geometry::printDistance() {
+	for (int i = 0; i <= num_points; i++) {
+		std::cout << i << std::endl;
+		std::cout << num_points << " : " << std::endl;
+		std::cout << point_array[i] << std::endl;
+	}
+}
+
+int main() {
+	Point p1(1, 3);
+	Geometry g1;
+	g1.addPoint(p1);
+	g1.printDistance();
+}
+*/
--- My first program/chapter4-5.cpp
+++ My first program/chapter4-5.cpp
@@ -1,33 +1,47 @@
 #include <iostream>
 #include <string.h>
 
-/*
 class MyString {
 	char* string_content; // 문자열 데이터를 가리키는 포인터
 	int string_length; // 문자열 길이
 	int memory_capacity; // 현재 할당된 용량
 
-	public:
-		// 문자 하나로 생성
-		MyString(char c);
+public:
+	// 문자 하나로 생성
+	MyString(char c);
 
-		// 문자열로부터 생성
-		MyString(const char* str);
+	// 문자열로부터 생성
+	MyString(const char* str);
 
-		// 복사 생성자
-		MyString(const MyString& str);
+	// 복사 생성자
+	MyString(const MyString& str);
 
-		~MyString();
+	~MyString();
 
-		int length() const;
-		int capacity() const;
-		void reserve(int size);
+	int length() const;
+	int capacity() const;
+	void reserve(int size);
 
-		void print() const;
-		void println() const;
+	void print() const;
+	void println() const;
 
-		MyString& assign(const MyString& str);
-		MyString& assign(const char* str);
+	MyString& assign(const MyString& str);
+	MyString& assign(const char* str);
+	
+	char at(int i) const;
+
+	MyString& insert(int loc, const MyString& str);
+	MyString& insert(int loc, const char* str);
+	MyString& insert(int loc, char c);
+
+	MyString& erase(int loc, int num);
+
+	int find(int find_from, MyString& str) const;
+	int find(int find_from, const char* str) const;
+	int find(int find_from, char c) const;
+
+	int compare(const MyString& str) const;
+	int compare2(const MyString* str);
 
 };
 
@@ -83,7 +97,7 @@ MyString& MyString::assign(const MyString& str) {
 		string_content = new char[str.string_length];
 		memory_capacity = str.string_length;
 	}
-	
+
 	for (int i = 0; i != str.string_length; i++) {
 		string_content[i] = str.string_content[i];
 	}
@@ -95,7 +109,7 @@ MyString& MyString::assign(const MyString& str) {
 
 MyString& MyString::assign(const char* str) {
 	int str_length = strlen(str);
-	if (str_length > memory_capacity) {
+	if (str_length > string_length) {
 		delete[] string_content;
 
 		string_content = new char[str_length];
@@ -111,6 +125,7 @@ MyString& MyString::assign(const char* str) {
 }
 
 int MyString::capacity() const { return memory_capacity; }
+
 void MyString::reserve(int size) {
 	if (size > memory_capacity) {
 		char* prev_string_content = string_content;
@@ -119,18 +134,181 @@ void MyString::reserve(int size) {
 		memory_capacity = size;
 
 		for (int i = 0; i != string_length; i++) {
-
+			string_content[i] = prev_string_content[i];
 		}
+
+		delete[] prev_string_content;
 	}
 }
 
-int main() {
-	MyString str1("hello world!");
-	MyString str2(str1);
-
-	str1.println();
-	str2.println();
-
-	
+char MyString::at(int i) const {
+	if (i >= string_length || i < 0) {
+		return NULL;
+	}
+	else return string_content[i];
 }
-*/
+
+MyString& MyString::insert(int loc, const MyString& str) {
+	// 범위를 벗어나는 입력에 대해서는 삽입을 수행하지 않음
+	if (loc < 0 || loc > string_length) return *this;
+
+	if (string_length + str.string_length > memory_capacity) {
+		// 새롭게 동적 할당
+		if (memory_capacity * 2 > string_length + str.string_length) memory_capacity *= 2;
+		else memory_capacity = string_length + str.string_length;
+
+		char* prev_string_content = string_content;
+		string_content = new char[memory_capacity];
+
+		// insert 되는 부분 직전까지 내용 복사
+		int i;
+		for (i = 0; i < loc; i++) {
+			string_content[i] = prev_string_content[i];
+		}
+
+		// 새롭게 insert 되는 문자열 삽입
+		for (int j = 0; j != str.string_length; j++) {
+			string_content[i + j] = str.string_content[j];
+		}
+
+		// 원 문자열의 나머지 뒷부분 복사
+		for (; i < string_length; i++) {
+			string_content[str.string_length + i] = prev_string_content[i];
+		}
+
+		delete[] prev_string_content;
+
+		string_length = string_length + str.string_length;
+		return *this;
+	}
+
+	// 만약 초과하지 않는다면 굳이 동적할당 안해도 됨
+	for (int i = string_length - 1; i >= loc; i--) {
+		string_content[i + str.string_length] = string_content[i];
+	}
+	for (int i = 0; i < str.string_length; i++) {
+		string_content[i + loc] = str.string_content[i];
+
+		string_length = string_length + str.string_length;
+		return *this;
+	}
+}
+
+MyString& MyString::insert(int loc, const char* str) {
+	MyString temp(str);
+	return insert(loc, temp);
+}
+
+MyString& MyString::insert(int loc, char c) {
+	MyString temp(c);
+	return insert(loc, temp);
+}
+
+MyString& MyString::erase(int loc, int num) {
+	// loc의 앞 부터 시작해 num 문자를 지움
+	if (num < 0 || loc < 0 || loc > string_length) return *this;
+
+	if (num > string_length) num = string_length;
+
+	for (int i = loc + num; i < string_length; i++) {
+		string_content[i - num] = string_content[i];
+	}
+
+	string_length -= num;
+	return *this;
+}
+
+int MyString::find(int find_from, MyString& str) const {
+	int i, j;
+	if (str.string_length == 0) return -1;
+	for (i = find_from; i <= string_length - str.string_length; i++) {
+		for (j = 0; j < str.string_length; j++) {
+			if (string_content[i + j] != str.string_content[j]) break;
+		}
+
+		if (j == str.string_length) return i;
+	}
+
+	return -1;
+}
+int MyString::find(int find_from, const char* str) const {
+	MyString temp(str);
+	return find(find_from, temp);
+}
+
+int MyString::find(int find_from, char c) const {
+	MyString temp(c);
+	return find(find_from, temp);
+}
+
+int MyString::compare(const MyString& str) const {
+	
+	for (int i = 0; i < std::min(string_length, str.string_length); i++) {
+		if (string_content[i] > str.string_content[i]) {
+			printf("%d, %d \n", string_content[i], str.string_content[i]);
+			return 1;
+		}
+		else if (string_content[i] < str.string_content[i])
+			return -1;
+	}
+
+	if (string_length == str.string_length) return 0;
+
+	else if (string_length < str.string_length) return 1;
+
+	return -1;
+}
+
+int MyString::compare2(const MyString* str) {
+
+	if (str == nullptr) {
+		return 1;
+	}
+
+	for (int i = 0; i < std::min(string_length, str->string_length); i++) {
+		if (string_content[i] > str->string_content[i]) {
+			printf("%d, %d \n", string_content[i], str->string_content[i]);
+			return 1;
+		}
+		else if (string_content[i] < str->string_content[i])
+			return -1;
+	}
+
+	if (string_length == str->string_length) return 0;
+
+	else if (string_length < str->string_length) return 1;
+
+	return -1;
+}
+
+int main() {
+	MyString str1("very very very long stirng");
+	MyString str2("<some string inserted between>");
+	str1.reserve(30);
+
+	std::cout << "Capacity : " << str1.capacity() << std::endl;
+	std::cout << "String length : " << str1.length() << std::endl;
+	str1.println();
+	std::cout << std::endl;
+
+	str1.insert(5, str2);
+	std::cout << "Capacity : " << str1.capacity() << std::endl;
+	std::cout << "String length : " << str1.length() << std::endl;
+	str1.println();
+	std::cout << std::endl;
+
+	MyString str3("this is a very very long string");
+	std::cout << "Location of first <very> in the stirng : " << str3.find(0, "very") << std::endl;
+	std::cout << "Location of second <very> in the string : " << str3.find(str3.find(0, "very") + 1, "very") << std::endl;
+
+	MyString str4("abc");
+	MyString str5("ABC");
+
+	std::cout << "compare : " << str4.compare(str5) << std::endl;
+
+	str5.erase(2, 4);
+	std::cout << "Capacity : " << str5.capacity() << std::endl;
+	std::cout << "String length : " << str5.length() << std::endl;
+	str5.println();
+
+}
