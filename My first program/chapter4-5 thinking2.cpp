ea4e2de9ec2bcda8f1013e659eccc54322b3bd19
s5hy25ni <s5hy25ni@gmail.com>
2022-12-09 17:55:25 +0900

    2022-12-09

--- My first program/chapter4-5 thinking2.cpp
+++ My first program/chapter4-5 thinking2.cpp
@@ -42,19 +42,43 @@ public:
 		delete[] str;
 	}
 
+	void badCharSearch(const string& txt, const string& pat) {
+		
+	}
 	void find(const string& s) {
-		if (len < s.len) { std::cout << "검색 대상보다 짧게 입력" << std::endl; }
+		if (len < s.len) { std::cout << "pattern is too longer" << std::endl; }
 		else {
-			int find_num = s.len - 1;
-			int same = 0;
-			for (int i = find_num; i > find_num-s.len; i--) {
-				if (str[i] == s.str[i]) {
-					std::cout << i << "번째 글자 일치" << std::endl;
+			std::cout << "START : find pattern(" << s.str << ") in text(" << str << ")" << std::endl << std::endl;
+
+			// badChar 전처리
+			int badChar[256];
+			for (int i = 0; i < 256; i++) { // 모든 문자 -1로 초기화
+				badChar[i] = -1;
+			}
+			for (int i = 0; i < s.len; i++) { // s.str에 있는 문자의 인덱스 저장(중복시 가장 오른쪽 인덱스)
+				badChar[(int)s.str[i]] = i;
+			}
+
+			int start = 0; // 검색 시작 인덱스
+			int pat_num = 0;
+
+			while (start <= (len - s.len)) { // 검색 시작 인덱스가 (s.len - len) 보다 클 수 없음
+				int j = s.len - 1; // 문자열 끝에서부터 검색하기 위함
+				while (j >= 0 && s.str[j] == str[start + j]) { // 
+					j--;
+				}
+				if (j < 0) {
+					pat_num++;
+					std::cout << pat_num << " pattern : str[" << start << "] ~ [" << start+s.len-1 << "]" << std::endl;
+					start += (start + s.len < len) ? s.len - badChar[str[start + s.len]] : 1;
 				}
 				else {
-					std::cout << i << "번째 글자 불일치" << std::endl;
+					start += std::max(1, j - badChar[str[start + j]]);
 				}
 			}
+
+			if (pat_num == 0) std::cout << "END : pattern is not in text" << std::endl;
+			else std::cout << std::endl << "END : " << pat_num << " pattern is in text" << std::endl;
 
 		}
 	}
@@ -62,7 +86,7 @@ public:
 
 int main() {
 	string a = "hello, sohyeon!";
-	string b = "soh"; 
+	string b = "o"; 
 
 	a.find(b);
 }
