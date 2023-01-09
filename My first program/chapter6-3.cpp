fe8b840d0fbef7453d8c2d22c249a3aa4eb55087
s5hy25ni <s5hy25ni@gmail.com>
2023-01-09 17:49:21 +0900

    2023-01-09

--- My first program/chapter6-3.cpp
+++ My first program/chapter6-3.cpp
@@ -1,23 +1,56 @@
 #include <iostream>
 
+//class Parent {
+//public:
+//	Parent() { std::cout << "Parent 생성자 호출" << std::endl; }
+//	virtual ~Parent() { std::cout << "Parent 소멸자 호출" << std::endl; }
+//};
+//
+//class Child : public Parent {
+//public:
+//	Child() :Parent() { std::cout << "Child 생성자 호출" << std::endl; }
+//	~Child() { std::cout << "Child 소멸자 호출" << std::endl; }
+//};
+//
+//int main() {
+//	std::cout << "=== 평범한 Child 만들었을 때 ===" << std::endl;
+//	{ Child c; }
+//	std::cout << "=== Parent 포인터로 Child 가리켰을 때 ===" << std::endl;
+//	{
+//		Parent* p = new Child();
+//		delete p;
+//	}
+//}
+
+//class A {
+//public:
+//	virtual void show() { std::cout << "Parent!" << std::endl; }
+//};
+//
+//class B : public A {
+//public:
+//	void show() override { std::cout << "Child!" << std::endl; }
+//};
+//
+//void test(A& a) { a.show(); }
+//
+//int main() {
+//	A a;
+//	B b;
+//	test(a);
+//	test(b);
+//
+//	return 0;
+//}
+
 class Parent {
 public:
-	Parent() { std::cout << "Parent 생성자 호출" << std::endl; }
-	~Parent() { std::cout << "Parent 소멸자 호출" << std::endl; }
+	virtual void func1();
+	virtual void func2();
 };
 
 class Child : public Parent {
 public:
-	Child() :Parent() { std::cout << "Child 생성자 호출" << std::endl; }
-	~Child() { std::cout << "Child 소멸자 호출" << std::endl; }
+	virtual void func1();
+	void func3();
 };
-
-int main() {
-	std::cout << "=== 평범한 Child 만들었을 때 ===" << std::endl;
-	{ Child c; }
-	std::cout << "=== Parent 포인터로 Child 가리켰을 때 ===" << std::endl;
-	{
-		Parent* p = new Child();
-		delete p;
-	}
-}
